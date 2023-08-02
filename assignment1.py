afe9f60431ed88c837d42c2b33982d15399d7799
rabinghimire2023 <rabin.ghimire@fusemachines.com>
2023-08-02 14:58:41 +0545

    Division operation done

--- assignment1.py
+++ assignment1.py
@@ -4,3 +4,8 @@ def subtraction(x,y):
     return x-y
 def multiplication(x,y):
     return x*y
+def division(x,y):
+    if y!=0:
+        return x/y
+    else:
+        raise ValueError("Division By zero!")
